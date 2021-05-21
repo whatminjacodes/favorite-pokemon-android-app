3648b04b0949c128fa263305cf8e11614ea5581a
whatminjacodes <whatminjacodes@gmail.com>
2021-05-21 15:40:55 +0300

    Added view binding

--- app/src/main/java/com/minjee/favoritepokemon/ui/MainActivity.kt
+++ app/src/main/java/com/minjee/favoritepokemon/ui/MainActivity.kt
@@ -1,27 +1,23 @@
 package com.minjee.favoritepokemon.ui
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.minjee.favoritepokemon.R
+import com.minjee.favoritepokemon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigationAllPokemon, R.id.navigationFavorites, R.id.navigationSettings))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val navigationView = binding.navView
+        val navigationController = findNavController(R.id.nav_host_fragment)
+
+        navigationView.setupWithNavController(navigationController)
     }
 }
